03a307ea466d26a00e5edb82f54cdbb0327cda27
IhorPerelyhin <arkhangel.ek@gmail.com>
2020-09-12 16:57:14 +0300

    test CustomerCell

--- MyPlaces/ViewController.swift
+++ MyPlaces/ViewController.swift
@@ -19,7 +19,6 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return restaurantNames.count
-        
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
